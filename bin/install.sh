ce43f50fbd815126d43dd1f007a3ee5c8b23843a
Andreas Hirschauer <andreas@hirschauer-it.de>
2016-12-26 01:38:17 +0100

    Update

--- bin/install.sh
+++ bin/install.sh
@@ -2,7 +2,7 @@
 set -e
 
 # install.sh
-#	This script installs my basic setup for a fedora laptop
+#	This script installs my basic setup for a arch laptop
 
 USERNAME=ahirschauer
 
@@ -16,69 +16,65 @@ check_is_sudo() {
 # sets up apt sources
 # assumes you are going to use debian stretch
 setup_sources() {
-	dnf update
-
-	# RPM Fusion Free/Nonfree
-	dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-
-	# neovim
-	dnf copr enable -y dperson/neovim
-
-	# syncthing
-	dnf copr enable -y decathorpe/syncthing
+	pacman -Syu
 }
 
 # install stuff for i3 window manager
 install_wmapps() {
-	local pkgs=( feh i3 i3lock i3status scrot lightdm neovim )
+	local pkgs=( feh i3 i3lock i3status scrot neovim )
 
-	dnf install -y "${pkgs[@]}"
+	pacman -S --needed --noconfirm "${pkgs[@]}"
 }
 
 base() {
 	# base development packages
-	dnf group install -y "Development Tools" "Development Libraries"
+	pacman -S --needed --noconfirm base-devel
 
 	# extra needed packages
-	dnf install -y \
+	pacman -S --needed --noconfirm \
+		arc-gtk-theme \
+		arc-icon-theme \
 		asciinema \
-		neovim \
 		jq \
+		lolcat \
+		neovim \
 		ranger \
 		syncthing \
 		syncthing-inotify \
 		powertop \
-		xbacklight \
-		xcalib \
+		xorg-xbacklight \
 		zsh
 
-	# install tlp with recommends
-	dnf install -y tlp tlp-rdw
+	# install powertop and tlp with recommends
+	pacman -S --needed --noconfirm powertop tlp tlp-rdw
 
-	dnf autoremove
-	dnf clean all
+	pacman -Sc
 
 	setup_sudo
 
 	install_shell
 	install_docker
-	install_scripts
 	install_syncthing
 }
 
 setup_sudo() {
 	# add user to systemd group
-	gpasswd -a "$USERNAME" systemd-journal
-	gpasswd -a "$USERNAME" systemd-network
+	usermod -aG systemd-journal "$USERNAME"
+	usermod -aG systemd-network "$USERNAME"
 
 	# add go path to secure path
 	{ \
-		echo -e 'Defaults	secure_path="/usr/local/go/bin:/home/ahirschauer/.go/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"'; 
-\
+		echo -e 'Defaults	secure_path="/usr/local/go/bin:/home/ahirschauer/.go/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"'; \
 		echo -e 'Defaults	env_keep += "ftp_proxy http_proxy https_proxy no_proxy GOPATH EDITOR"'; \
 		echo -e "${USERNAME} ALL=(ALL) NOPASSWD:ALL"; \
 		echo -e "${USERNAME} ALL=NOPASSWD: /sbin/ifconfig, /sbin/ifup, /sbin/ifdown, /sbin/ifquery"; \
 	} >> /etc/sudoers
+
+	# setup downloads folder as tmpfs
+	# that way things are removed on reboot
+	# i like things clean but you may not want this
+	mkdir -p "/home/$USERNAME/Downloads"
+	echo -e "\n# tmpfs for downloads\ntmpfs\t/home/${USERNAME}/Downloads\ttmpfs\tnodev,nosuid,size=2G\t0\t0" >> /etc/fstab
 }
 
 install_shell() {
@@ -118,42 +114,19 @@ get_dotfiles() {
 	)
 }
 
-# installs docker master
-# and adds necessary items to boot params
-install_docker() {
-	# create docker group
-	sudo groupadd docker
-	sudo usermod -aG docker "$USERNAME"
+install_vim() {
+	curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+}
 
-	dnf group install -y "Container-Management"
+# installs docker
+install_docker() {
+	pacman -S docker
+
+	usermod -aG docker "$USERNAME"
 
 	systemctl daemon-reload
 	systemctl enable docker
-}
-
-
-# install custom scripts/binaries
-install_scripts() {
-	# install speedtest
-	curl -sSL https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest_cli.py > /usr/local/bin/speedtest
-	chmod +x /usr/local/bin/speedtest
-
-	# install icdiff
-	curl -sSL https://raw.githubusercontent.com/jeffkaufman/icdiff/master/icdiff > /usr/local/bin/icdiff
-	curl -sSL https://raw.githubusercontent.com/jeffkaufman/icdiff/master/git-icdiff > /usr/local/bin/git-icdiff
-	chmod +x /usr/local/bin/icdiff
-	chmod +x /usr/local/bin/git-icdiff
-
-	# install lolcat
-	curl -sSL https://raw.githubusercontent.com/tehmaze/lolcat/master/lolcat > /usr/local/bin/lolcat
-	chmod +x /usr/local/bin/lolcat
-
-#	local scripts=( go-md2man have light )
-#
-#	for script in "${scripts[@]}"; do
-#		curl -sSL "https://misc.j3ss.co/binaries/$script" > "/usr/local/bin/${script}"
-#		chmod +x "/usr/local/bin/${script}"
-#	done
 }
 
 # install syncthing
@@ -165,6 +138,8 @@ install_syncthing() {
 
 # install/update golang from source
 install_golang() {
+	sudo pacman -S go
+
 	# get commandline tools
 	(
 	set -x
@@ -209,44 +184,15 @@ install_golang() {
 	go get github.com/shurcooL/gostatus
 	go get github.com/shurcooL/markdownfmt
 	go get github.com/Soulou/curl-unix-socket
+}
 
-	aliases=( cloudflare/cfssl docker/docker letsencrypt/boulder opencontainers/runc jessfraz/binctr jessfraz/contained.af )
-	for project in "${aliases[@]}"; do
-		owner=$(dirname "$project")
-		repo=$(basename "$project")
-		if [[ -d "${HOME}/${repo}" ]]; then
-			rm -rf "${HOME:?}/${repo}"
-		fi
+install_kvm() {
+	pacman -S --needed --noconfirm qemu virt-manager virt-viewer dnsmasq iptables ebtables vde2 bridge-utils openbsd-netcat
 
-		mkdir -p "${GOPATH}/src/github.com/${owner}"
+	usermod -aG kvm "$USERNAME"
 
-		if [[ ! -d "${GOPATH}/src/github.com/${project}" ]]; then
-			(
-			# clone the repo
-			cd "${GOPATH}/src/github.com/${owner}"
-			git clone "https://github.com/${project}.git"
-			# fix the remote path, since our gitconfig will make it git@
-			cd "${GOPATH}/src/github.com/${project}"
-			git remote set-url origin "https://github.com/${project}.git"
-			)
-		else
-			echo "found ${project} already in gopath"
-		fi
-
-		# make sure we create the right git remotes
-		if [[ "$owner" != "jessfraz" ]]; then
-			(
-			cd "${GOPATH}/src/github.com/${project}"
-			git remote set-url --push origin no_push
-			git remote add jessfraz "https://github.com/jessfraz/${repo}.git"
-			)
-		fi
-	done
-
-	# do special things for k8s GOPATH
-	mkdir -p "${GOPATH}/src/k8s.io"
-	git clone "https://github.com/kubernetes/kubernetes.git" "${GOPATH}/src/k8s.io/kubernetes"
-	)
+	systemctl daemon-reload
+	systemctl enable libvirtd
 }
 
 usage() {
@@ -258,7 +204,7 @@ usage() {
 	echo "  dotfiles                    - get dotfiles"
 	echo "  vim                         - install vim specific dotfiles"
 	echo "  golang                      - install golang and packages"
-	echo "  scripts                     - install scripts"
+	echo "  kvm                         - install kvm
 	echo "  syncthing                   - install syncthing"
 }
 
@@ -279,7 +225,7 @@ main() {
 	elif [[ $cmd == "wm" ]]; then
 		check_is_sudo
 
-		setup_sudo
+		install_wmapps
 	elif [[ $cmd == "shell" ]]; then
 		install_shell
 	elif [[ $cmd == "dotfiles" ]]; then
@@ -288,8 +234,10 @@ main() {
 		install_vim
 	elif [[ $cmd == "golang" ]]; then
 		install_golang "$2"
-	elif [[ $cmd == "scripts" ]]; then
-		install_scripts
+    elif [[ $cmd == "kvm" ]]; then
+		check_is_sudo
+
+		install_kvm
 	elif [[ $cmd == "syncthing" ]]; then
 		install_syncthing
 	else
